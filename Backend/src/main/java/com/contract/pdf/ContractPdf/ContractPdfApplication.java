e9bfdc59d07dedb03b9dcf15553f71cc84325cf5
Carlos S. Ferreira <ferreira.carlos.bcs@gmail.com>
2025-09-04 07:24:10 -0300

    feat: initial backend config

---
+++ Backend/src/main/java/com/contract/pdf/ContractPdf/ContractPdfApplication.java
@@ -0,0 +1,13 @@
+package com.contract.pdf.ContractPdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractPdfApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/contract/pdf/ContractPdf/ContractPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contract.pdf.ContractPdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
